39c7de361bb549cd9d3009051a501f4bfad9e66c
Akash S <akashspalloor@gmail.com>
2025-01-17 16:52:11 +0530

    crud successfully created

--- controller/contactController.js
+++ controller/contactController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler")
 const Contact = require("../models/contactModel")
 // Get all contacts
 // route -> GET/api/contacts
-const getContact = asyncHandler(async(req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     const contacts = await Contact.find()
     res.status(200).json(contacts)
 })
@@ -27,19 +27,40 @@ const createContact = asyncHandler(async (req, res) => {
 // GET contacts by id
 // route -> GET/api/contacts/:id
 const getContacts = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Get contacts for ${req.params.id}` })
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    res.status(200).json(contact)
 })
 
 // Update contacts
 // route -> PUT/api/contacts/:id
 const UpdateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}` })
+    const contact = await Contact.findByIdAndUpdate(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    )
+    res.status(200).json(updatedContact)
 })
 
 // Delete contacts
 // route -> DELETE/api/contacts/:id
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete contact for ${req.params.id}` })
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    await Contact.findByIdAndDelete(req.params.id)
+    res.status(200).json(contact)
 })
 
 module.exports = { getContact, createContact, getContacts, UpdateContact, deleteContact }
